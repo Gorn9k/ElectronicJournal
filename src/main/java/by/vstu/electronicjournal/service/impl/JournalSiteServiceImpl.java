068be03c021d653214c8c07d6d8e9f1bd07e2e36
KovalevKA <kss47242@gmail.com>
2022-02-16 20:29:48 +0300

    update generator journals

--- src/main/java/by/vstu/electronicjournal/service/impl/JournalSiteServiceImpl.java
+++ src/main/java/by/vstu/electronicjournal/service/impl/JournalSiteServiceImpl.java
@@ -16,6 +16,8 @@ import by.vstu.electronicjournal.service.GroupService;
 import by.vstu.electronicjournal.service.JournalSiteService;
 import by.vstu.electronicjournal.service.TeacherService;
 import by.vstu.electronicjournal.service.common.impl.CommonCRUDServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -23,76 +25,79 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class JournalSiteServiceImpl
-        extends CommonCRUDServiceImpl<JournalSite, JournalSiteDTO, JournalSiteRepository>
-        implements JournalSiteService {
+	extends CommonCRUDServiceImpl<JournalSite, JournalSiteDTO, JournalSiteRepository>
+	implements JournalSiteService {
 
-    @Value("${entrance.timetable}")
-    private String path;
+	@Value("${entrance.timetable}")
+	private String path;
 
-    @Autowired
-    private Mapper mapper;
+	@Autowired
+	private Mapper mapper;
 
-    @Autowired
-    private JournalSiteRepository journalSiteRepository;
+	@Autowired
+	private JournalSiteRepository journalSiteRepository;
 
-    @Autowired
-    private DisciplineService disciplineService;
+	@Autowired
+	private DisciplineService disciplineService;
 
-    @Autowired
-    private TeacherService teacherService;
+	@Autowired
+	private TeacherService teacherService;
 
-    @Autowired
-    private GroupService groupService;
+	@Autowired
+	private GroupService groupService;
 
-    public JournalSiteServiceImpl() {
-        super(JournalSite.class, JournalSiteDTO.class);
-    }
+	public JournalSiteServiceImpl() {
+		super(JournalSite.class, JournalSiteDTO.class);
+	}
 
-    @Override
-    public List<JournalSiteDTO> search(String query) {
-        if (query.isEmpty()) {
-            return findAll();
-        }
-        return mapper.toDTOs(journalSiteRepository.findAll(getSpecifications(query)), JournalSiteDTO.class);
-    }
+	@Override
+	public List<JournalSiteDTO> search(String query) {
+		if (query.isEmpty()) {
+			return findAll();
+		}
+		return mapper
+			.toDTOs(journalSiteRepository.findAll(getSpecifications(query)), JournalSiteDTO.class);
+	}
 
-    @Override
-    public List<JournalSite> generate() {
+	@Override
+	public List<JournalSite> generate(List<JournalSite> sites) {
 
-        RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
 
-        String queryToCommonInfo = String.format("%s/patterns/", path);
-        List<PatternDTO> patternDTOS =
-                restTemplate.exchange(queryToCommonInfo, HttpMethod.GET, null, new ParameterizedTypeReference<List<PatternDTO>>() {
-                }).getBody();
+		String queryToCommonInfo = String.format("%s/patterns/", path);
+		List<PatternDTO> patternDTOS =
+			restTemplate.exchange(queryToCommonInfo, HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<PatternDTO>>() {
+				}).getBody();
 
-        List<JournalSite> result = new ArrayList<>();
+		List<JournalSite> result = new ArrayList<>();
 
-        for (PatternDTO patternDTO : patternDTOS) {
+		for (PatternDTO patternDTO : patternDTOS) {
 
-            DisciplineDTO disciplineDTO = disciplineService.validator("name==\'" + patternDTO.getDisciplineName() + "\'").get(0);
-            TeacherDTO teacherDTO = teacherService.validator(parsingFIOTeacher(patternDTO.getTeacherFio())).get(0);
-            GroupDTO groupDTO = groupService.validator("name==\'" + patternDTO.getGroupName() + "\'").get(0);
+			DisciplineDTO disciplineDTO = disciplineService
+				.validator("name==\'" + patternDTO.getDisciplineName() + "\'").get(0);
+			TeacherDTO teacherDTO = teacherService
+				.validator(parsingFIOTeacher(patternDTO.getTeacherFio())).get(0);
+			GroupDTO groupDTO = groupService
+				.validator("name==\'" + patternDTO.getGroupName() + "\'").get(0);
 
-            JournalSite journalSite = new JournalSite();
-            journalSite.setDiscipline((Discipline) mapper.toEntity(disciplineDTO, Discipline.class));
-            journalSite.setTeacher((Teacher) mapper.toEntity(teacherDTO, Teacher.class));
-            journalSite.setGroup((Group) mapper.toEntity(groupDTO, Group.class));
+			JournalSite journalSite = new JournalSite();
+			journalSite
+				.setDiscipline((Discipline) mapper.toEntity(disciplineDTO, Discipline.class));
+			journalSite.setTeacher((Teacher) mapper.toEntity(teacherDTO, Teacher.class));
+			journalSite.setGroup((Group) mapper.toEntity(groupDTO, Group.class));
 
-            result.add(journalSite);
-        }
+			sites.add(journalSite);
+		}
 
-        return journalSiteRepository.saveAll(result);
-    }
+		return journalSiteRepository.saveAll(sites);
+	}
 
-    private String parsingFIOTeacher(String fio) {
-        fio = fio.replace("\'", "").replace(".", " ");
-        String[] temp = fio.split(" ");
-        return String.format("surname==%s;name==%s*;patronymic==%s*", temp[0], temp[1], temp[2]);
-    }
+	private String parsingFIOTeacher(String fio) {
+		fio = fio.replace("\'", "").replace(".", " ");
+		String[] temp = fio.split(" ");
+		return String.format("surname==%s;name==%s*;patronymic==%s*", temp[0], temp[1], temp[2]);
+	}
 }
